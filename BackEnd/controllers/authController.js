3c11b6af01e7e96db8cc7831a6f300a3878dc173
aarrrryyaaaaa <aryatonys01@gmail.com>
2026-08-06 14:51:42 +0800

    Update JWT token expiration to 5 hours

--- BackEnd/controllers/authController.js
+++ BackEnd/controllers/authController.js
@@ -32,7 +32,7 @@ exports.login = async (req, res) => {
         }
 
         const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, {
-            expiresIn: '10h'
+            expiresIn: '5h'
         });
 
         const { password: _, ...userWithoutPassword } = user;
